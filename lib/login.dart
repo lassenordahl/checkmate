400b1b21110bc64943cd01066e2d6988409cf711
Lasse Nordahl <lasseanordahl@gmail.com>
2020-02-16 20:19:52 -0800

    hm

--- lib/login.dart
+++ lib/login.dart
@@ -9,6 +9,9 @@ import 'package:crypto/crypto.dart';
 import 'package:uni_links/uni_links.dart';
 import 'package:flutter/services.dart' show PlatformException, SystemNavigator;
 
+import 'package:http/http.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 void main() => runApp(Login());
 
 class Login extends StatelessWidget {
@@ -47,6 +50,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _launchURL() async {
     verifier = _generateCodeVerifier();
+    //Store verifier on disk
+    // obtain shared preferences
+    final prefs = await SharedPreferences.getInstance();
+    // set value
+    prefs.setString('verifier', verifier);
+
     var codeChallenge = _generateCodeChallenge(verifier);
     print("Code Verifier:" + verifier);
 
@@ -91,6 +100,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   String _link;
+  String _authCode;
+  String _authToken;
+  String _authTokenResponse;
   StreamSubscription _sub;
 
   @override
@@ -109,6 +121,14 @@ class _MyHomePageState extends State<MyHomePage> {
       setState(() {
         _link = initialLink;
       });
+      if(_link != null)
+      {
+        String code = _link.split("=")[1];
+        _authCode = code;
+        print(code); // E4t8E7TIwhIK97wr
+        _exchangeAuthForToken();
+      }
+      
     } on PlatformException {
       // Handle exception by warning the user their action did not succeed
       // return?
@@ -120,10 +140,79 @@ class _MyHomePageState extends State<MyHomePage> {
       setState(() {
         _link = uri.toString();
       });
+      if(_link != null)
+      {
+        String code = _link.split("=")[1];
+        _authCode = code;
+        _exchangeAuthForToken();
+      }
     }, onError: (err) {
       // Handle exception by warning the user their action did not succeed
     });
 
+  }
+
+  //Trade Auth code for token
+  _exchangeAuthForToken() async {
+    //Read from disk
+    final prefs = await SharedPreferences.getInstance();
+
+    // Try reading data from the counter key. If it doesn't exist, return 0.
+    var verifierDisk = prefs.getString('verifier');
+
+    print('verfier:');
+    print(verifierDisk);
+    print('##################');
+    var codeChallenge = _generateCodeChallenge(verifierDisk);
+
+    /*
+      * @apiParam {String} type Whether the token is for "login" or "refresh".
+      * @apiParam {String} client_id Client ID for the user pool.
+      * @apiParam {String} refresh_token Refresh token. Only required for the "refresh" type.
+      * @apiParam {String} code Code from the OAuth2 provider. Required only for "login" type.
+      * @apiParam {String} code_verifier Code verifier for login. Required only for "login" type.
+      * @apiParam {String} redirect_uri Redirects to this location once completed. Required only for "login" type.
+    */
+
+    // set up POST request arguments
+    String url = 'https://bttmns45mb.execute-api.us-west-2.amazonaws.com/development/auth/tokens';
+    /*+'?type=login'
+    + '&client_id=NHoUARv7KKdO2VcCud3OWzpvZ52b16m8'
+    + '&code=' + _authCode
+    + '&code_challenge=' + codeChallenge
+    + '&redirect_uri=deeplink://testing';*/
+
+    print(url);
+
+    Map<String, String> headers = {"Content-type": "application/json"};
+    var jsonBody = {};
+    jsonBody["type"] = "login";
+    jsonBody["client_id"] = "NHoUARv7KKdO2VcCud3OWzpvZ52b16m8";
+    jsonBody["code"] = _authCode;
+    jsonBody["code_challenge"] = codeChallenge;
+    jsonBody["redirect_uri"] = "deeplink://testing";
+    String bodyJson = json.encode(jsonBody);
+    /*Map<String, String> json = {"type": "login", "client_id": "NHoUARv7KKdO2VcCud3OWzpvZ52b16m8",
+    "code": _authCode, "code_challenge": codeChallenge, "redirect_uri":"deeplink://testing"
+    };*/
+    /*String json = '{"type": "login", "client_id": "NHoUARv7KKdO2VcCud3OWzpvZ52b16m8", "code": "' 
+    + _authCode + '", "code_challenge": "'+ codeChallenge + '", "redirect_uri": "deeplink://testing"}';*/
+
+    // make POST request
+    Response response = await post(url, headers: headers, body: bodyJson);
+
+    // check the status code for the result
+    //int statusCode = response.statusCode;
+    
+    // this API passes back the id of the new item added to the body
+    String body = response.body;
+    print(body);
+    //_authTokenResponse = body;
+    //var parsedJson = json.decode(response.body);
+    //_authToken = parsedJson['data']['access_token'];
+
+    //Get Authtoken from repsponse
+    
   }
 
   @override
@@ -142,6 +231,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             Text(_link ?? ""),
+            Text(_authCode ?? ""),
+            Text(_authToken ?? ""),
           ]
         ),
       ),
