3d041e1b8bcadfeaceeec707ff71039e83fe6be3
Lasse Nordahl <lasseanordahl@gmail.com>
2020-03-08 00:27:32 -0800

    timepicker

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -3,9 +3,11 @@ import 'dart:async';
 import 'dart:convert';
 
 import '../objects/Task.dart';
+import '../objects/ScheduleTime.dart';
 
 String awsUrl = "https://bttmns45mb.execute-api.us-west-2.amazonaws.com";
 String knnUrl = "https://checkmate-data-backend.herokuapp.com/api";
+String devUrl = "localhost:5000/api";
 String userId = "1";
 
 String authorizationHeader = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFqRTJNemN5TWpKRE1ERTFSRVE0UXpRME1UQkJOVVpDUlRreU1ESXhSVVpFTnpFd09EVTFOdyJ9.eyJpc3MiOiJodHRwczovL2xlYXJuaW5nY2FsZW5kYXItZGV2ZWxvcG1lbnQuYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTExMDEzNzc2NjY0NjI2MjA1OTY2IiwiYXVkIjpbImh0dHBzOi8vYnR0bW5zNDVtYi5leGVjdXRlLWFwaS51cy13ZXN0LTIuYW1hem9uYXdzLmNvbS9kZXZlbG9wbWVudCIsImh0dHBzOi8vbGVhcm5pbmdjYWxlbmRhci1kZXZlbG9wbWVudC5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNTgzNjQzMTA4LCJleHAiOjE1ODM3Mjk1MDgsImF6cCI6Ik5Ib1VBUnY3S0tkTzJWY0N1ZDNPV3pwdlo1MmIxNm04Iiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBvZmZsaW5lX2FjY2VzcyJ9.dJzf1IMNoXX7fL2myMjl-WcNo41mnNv38C4O6Xz7RS5Wij6cPl1mHxdJ-yJEmcnWASbHi61Hs3k5HpCZOklxlabDU8kJ3_wNtO7z7o3sXQ_zTx7Tn_YJYYLBVztu-06jJ8KSe05mfgiG6JZ8H_uDf_TS2egtrym_CrvPWVr-ON8Z9KT6qEjibNeeTFG0PXEMnuw7Ur_maR4PAg9h91xaOBEEKnQ7awMRz8k-6FfDYAmMUjDrgCdDcmQsclxnHKFOiNRuTE0A_i0qDJFXvar0CX_JfG4VuqZ2dgQ6IC9GjnJ70I9dX-KGhyHK3_Gm1jwGdBAQixYl_tCsYztS93V4Pw";
@@ -16,12 +18,25 @@ Map<String, String> requestHeaders = {
        'Authorization': authorizationHeader
      };
 
+
+Future<List<ScheduleTime>> getAnnotatedTimes(String taskType) async {
+  final response =
+      await http.get(knnUrl + '/1/times?task_type=' + taskType, headers: requestHeaders);
+
+  if (response.statusCode == 200) {
+    return json.decode(response.body).map<ScheduleTime>((time) => ScheduleTime.fromJson(time)).toList();
+  } else {
+    throw Exception("You done fucked up you dumb ho");
+  }
+}
+
+
 Future<List<Task>> getPast() async {
   final response =
       await http.get(awsUrl + '/development/task/past?completed=-1', headers: requestHeaders);
 
   if (response.statusCode == 200) {
-      return json.decode(response.body)['data'].map<Task>((task) => Task.fromJson(task)).toList();
+    return json.decode(response.body)['data'].map<Task>((task) => Task.fromJson(task)).toList();
   } else {
     // If the server did not return a 200 OK response, then throw an exception.
     throw Exception('Failed to get past task');
